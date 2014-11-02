e82d6d1a41ff9d367dc4a3a269401f73e90d6bae
Fixer1008 <madridist1008@gmail.com>
2014-11-02 22:15:17 +0300

    Ololo

--- Laba5_SPOLKS_Server/Program.cs
+++ Laba5_SPOLKS_Server/Program.cs
@@ -11,6 +11,7 @@ namespace Laba5_SPOLKS_Server
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Ololo!");
         }
     }
 }
