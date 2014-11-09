c8528a27a15fecaccdb8c5178af975d864413688
Fixer1008 <madridist1008@gmail.com>
2014-11-09 16:53:52 +0300

    Server chat

--- Laba5_SPOLKS_Server/Program.cs
+++ Laba5_SPOLKS_Server/Program.cs
@@ -10,28 +10,56 @@ namespace Laba5_SPOLKS_Server
 {
     class Program
     {
-        private static string Receive()
+        private static void Receive()
         {
+            const string EndMessage = "<End>";
+
             UdpClient receivingUdpClient = new UdpClient(11000);
+
+            IPAddress ipAddress = IPAddress.Parse("192.168.0.103");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 5001);
 
             IPEndPoint remoteEndPoint = null;
 
+            receivingUdpClient.Connect(ipEndPoint);
+
             try
             {
-                var receivedDatagram = receivingUdpClient.Receive(ref remoteEndPoint);
-                receivingUdpClient.Close();
-                return Encoding.UTF8.GetString(receivedDatagram);
+                while (true)
+                {
+                    var receivedDatagram = receivingUdpClient.Receive(ref remoteEndPoint);
+                    Console.WriteLine("Client: {0}", Encoding.UTF8.GetString(receivedDatagram));
+
+                    if (Encoding.UTF8.GetString(receivedDatagram) == EndMessage)
+                    {
+                        break;
+                    }
+
+                    Console.Write("Server: ");
+                    var sendMessage = Console.ReadLine();
+                    var sendBytes = receivingUdpClient.Send(Encoding.UTF8.GetBytes(sendMessage), sendMessage.Length);
+
+                    if (sendMessage == EndMessage)
+                    {
+                        break;
+                    }
+                }
             }
             catch (Exception e)
             {                
                 throw e;
             }
+            finally
+            {
+                receivingUdpClient.Close();
+            }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Waiting for message...");
-            Console.WriteLine("Received datagram: {0}", Receive());
+            Console.WriteLine("Waiting...");
+            Receive();
+            Console.WriteLine("Please, ");
             Console.ReadLine();
         }
     }
