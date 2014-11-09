89e877cd7219e2fa4067921a5b0e0b1af1dbdf63
Fixer1008 <madridist1008@gmail.com>
2014-11-09 14:44:21 +0300

    Simple UdpReceiveClient

--- Laba5_SPOLKS_Server/Program.cs
+++ Laba5_SPOLKS_Server/Program.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Laba5_SPOLKS_Server
 {
     class Program
     {
+        private static string Receive()
+        {
+            UdpClient receivingUdpClient = new UdpClient(11000);
+
+            IPEndPoint remoteEndPoint = null;
+
+            try
+            {
+                var receivedDatagram = receivingUdpClient.Receive(ref remoteEndPoint);
+                receivingUdpClient.Close();
+                return Encoding.UTF8.GetString(receivedDatagram);
+            }
+            catch (Exception e)
+            {                
+                throw e;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("Ololo!");
+            Console.WriteLine("Waiting for message...");
+            Console.WriteLine("Received datagram: {0}", Receive());
+            Console.ReadLine();
         }
     }
 }
