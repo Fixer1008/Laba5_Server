315f40969dc393534d1cbcec524b245dacec9d4a
Fixer1008 <madridist1008@gmail.com>
2014-11-23 22:53:37 +0300

    Add ReceiveFile function

--- Laba5_SPOLKS_Server/FileReceiver.cs
+++ Laba5_SPOLKS_Server/FileReceiver.cs
@@ -5,44 +5,96 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Xml.Serialization;
+using System.Diagnostics;
 
 namespace Laba5_SPOLKS_Server
 {
     public class FileReceiver
     {
-        private readonly UdpFileClient _udpFileClient;
-        private readonly FileStream _fileStream;
+        private readonly UdpFileClient _udpFileReceiver;
+        private readonly UdpFileClient _udpFileSender;
 
+        private FileStream _fileStream;
         private IPAddress _localIpAddress;
         private IPEndPoint _localIpEndPoint;
         private IPEndPoint _remoteIpEndPoint = null;
 
+        private FileDetails _fileDetails;
+
         public FileReceiver()
         {
-            _udpFileClient = new UdpFileClient(11000);
+            _udpFileReceiver = new UdpFileClient(11000);
+            _udpFileSender = new UdpFileClient();
         }
 
         public int ReceiveFrom()
         {
-            var fileDetails = ReceiveFileDetails();
-            Console.WriteLine(fileDetails.FileName);
-            Console.WriteLine(fileDetails.FileLength);
+            ReceiveFileDetails();
+
+            Console.WriteLine(_fileDetails.FileName);
+            Console.WriteLine(_fileDetails.FileLength);
+
+            ReceiveFileData();
+
             return 0;
         }
 
-        private FileDetails ReceiveFileDetails()
+        private int ReceiveFileDetails()
         {
-            var receivedFileInfo = _udpFileClient.Receive(ref _remoteIpEndPoint);
-            _udpFileClient.Close();
-
             MemoryStream memoryStream = new MemoryStream();
-            memoryStream.Write(receivedFileInfo, 0, receivedFileInfo.Length);
 
-            XmlSerializer serializer = new XmlSerializer(typeof(FileDetails));
+            try
+            {
+                var receivedFileInfo = _udpFileReceiver.Receive(ref _remoteIpEndPoint);
 
-            FileDetails fileDetails = (FileDetails)serializer.Deserialize(memoryStream);
+                XmlSerializer serializer = new XmlSerializer(typeof(FileDetails));
 
-            return fileDetails;
+                memoryStream.Write(receivedFileInfo, 0, receivedFileInfo.Length);
+                memoryStream.Position = 0;
+
+                _fileDetails = (FileDetails)serializer.Deserialize(memoryStream);
+                return 0;
+            }
+            catch (Exception e)
+            {
+                _udpFileReceiver.Close();
+                Console.WriteLine(e.Message);                
+            }
+            finally
+            {
+                memoryStream.Dispose();
+            }
+
+            return -1;
+        }
+
+        private int ReceiveFileData()
+        {
+            try
+            {
+                var fileDataArray = _udpFileReceiver.Receive(ref _remoteIpEndPoint);
+
+                if (fileDataArray.Length != 0)
+                {
+                    _fileStream = new FileStream(_fileDetails.FileName, FileMode.Create, FileAccess.Write);
+                    _fileStream.Write(fileDataArray, 0, fileDataArray.Length);
+
+                    Process.Start(_fileDetails.FileName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+            finally
+            {
+                _udpFileReceiver.Close();
+                _fileStream.Close();
+                _fileStream.Dispose();
+            }
+
+            return 0;
         }
     }
 }
