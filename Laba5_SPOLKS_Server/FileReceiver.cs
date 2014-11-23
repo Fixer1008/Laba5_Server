17491223c4e1204c403bb2ccb1f1d31a21eec68c
Fixer1008 <madridist1008@gmail.com>
2014-11-23 18:44:36 +0300

    Add FileReceiver class

---
+++ Laba5_SPOLKS_Server/FileReceiver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml.Serialization;
+
+namespace Laba5_SPOLKS_Server
+{
+    public class FileReceiver
+    {
+        private readonly UdpFileClient _udpFileClient;
+        private readonly FileStream _fileStream;
+
+        private IPAddress _localIpAddress;
+        private IPEndPoint _localIpEndPoint;
+        private IPEndPoint _remoteIpEndPoint = null;
+
+        public FileReceiver()
+        {
+            _udpFileClient = new UdpFileClient(11000);
+        }
+
+        public int ReceiveFrom()
+        {
+            var fileDetails = ReceiveFileDetails();
+            Console.WriteLine(fileDetails.FileName);
+            Console.WriteLine(fileDetails.FileLength);
+            return 0;
+        }
+
+        private FileDetails ReceiveFileDetails()
+        {
+            var receivedFileInfo = _udpFileClient.Receive(ref _remoteIpEndPoint);
+            _udpFileClient.Close();
+
+            MemoryStream memoryStream = new MemoryStream();
+            memoryStream.Write(receivedFileInfo, 0, receivedFileInfo.Length);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(FileDetails));
+
+            FileDetails fileDetails = (FileDetails)serializer.Deserialize(memoryStream);
+
+            return fileDetails;
+        }
+    }
+}
--- Laba5_SPOLKS_Server/Program.cs
+++ Laba5_SPOLKS_Server/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 
@@ -10,56 +7,25 @@ namespace Laba5_SPOLKS_Server
 {
     class Program
     {
-        private static void Receive()
-        {
-            const string EndMessage = "<End>";
-
-            UdpClient receivingUdpClient = new UdpClient(11000);
-
-            IPAddress ipAddress = IPAddress.Parse("192.168.0.103");
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 5001);
-
-            IPEndPoint remoteEndPoint = null;
-
-            receivingUdpClient.Connect(ipEndPoint);
-
-            try
-            {
-                while (true)
-                {
-                    var receivedDatagram = receivingUdpClient.Receive(ref remoteEndPoint);
-                    Console.WriteLine("Client: {0}", Encoding.UTF8.GetString(receivedDatagram));
-
-                    if (Encoding.UTF8.GetString(receivedDatagram) == EndMessage)
-                    {
-                        break;
-                    }
-
-                    Console.Write("Server: ");
-                    var sendMessage = Console.ReadLine();
-                    var sendBytes = receivingUdpClient.Send(Encoding.UTF8.GetBytes(sendMessage), sendMessage.Length);
-
-                    if (sendMessage == EndMessage)
-                    {
-                        break;
-                    }
-                }
-            }
-            catch (Exception e)
-            {                
-                throw e;
-            }
-            finally
-            {
-                receivingUdpClient.Close();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Waiting...");
-            Receive();
-            Console.WriteLine("Please, ");
+            var ipAddress = Dns.GetHostAddresses("ALEX-NOTE");
+
+            Console.WriteLine(ipAddress[1]);
+            Console.WriteLine("Waiting for file receiving...");
+
+            FileReceiver fileReceiver = new FileReceiver();
+            var result = fileReceiver.ReceiveFrom();
+
+            if (result == -1)
+            {
+                Console.WriteLine("Error!");
+            }
+            else
+            {
+                Console.WriteLine("Success!");
+            }
+
             Console.ReadLine();
         }
     }
